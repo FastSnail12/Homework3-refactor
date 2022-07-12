a6133d58b779e7df8af812042c77b44e375f054c
FastSnail12 <108716384+FastSnail12@users.noreply.github.com>
2022-07-12 18:09:10 +0300

    first

--- lecture3/task2/main.py
+++ lecture3/task2/main.py
@@ -12,7 +12,7 @@ def controlled_execution(call_count, start_sleep_time, factor, border_sleep_time
                 output = f'Запуск номер {count}. Ожидание: {t} секунд. Результат декорируемой функций = {func_result}.'
                 print(output)
                 sleep(t)
-                t *= (2 ** factor)
+                t *= factor
                 if t >= border_sleep_time:
                     t = border_sleep_time
             print('Конец работы')
@@ -22,7 +22,7 @@ def controlled_execution(call_count, start_sleep_time, factor, border_sleep_time
     return controlled_function
 
 
-@controlled_execution(5, 1, 1, 10)
+@controlled_execution(5, 1, 2, 10)
 def test(number):
     return number ** 2
 
